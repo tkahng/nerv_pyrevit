136ed2de27a929e88e3ca211e74686cb30271375
lmengfan <mengfan.lou@gmail.com>
2018-10-05 09:26:06 -0400

    Version 0.01

---
+++ CustomExtension.extension/STVTools.tab/rps_init.py
@@ -0,0 +1,30 @@
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Architecture import *
+from Autodesk.Revit.DB.Analysis import *
+from Autodesk.Revit.UI import *
+
+
+import clr
+clr. AddReferenceByPartialName('PresentationCore')
+clr.AddReferenceByPartialName('PresentationFramework')
+clr.AddReferenceByPartialName('System.Windows.Forms')
+
+import System.Windows
+
+
+uidoc = __revit__.ActiveUIDocument
+doc = __revit__.ActiveUIDocument.Document
+selection = [doc.GetElement(id)
+            for id in __revit__.ActiveUIDocument.Selection.GetElementIds()]
+
+
+def alert(msg):
+    TaskDialogue.Show('pyRevit', msg)
+
+
+def quit():
+    __window__.Close()
+
+
+if len(selection) > 0:
+    el = selection[0]
