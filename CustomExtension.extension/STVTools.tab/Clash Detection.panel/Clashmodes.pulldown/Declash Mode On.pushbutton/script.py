2fa6a61ef9fa44d5cec2d345dcfe60a979aa60c7
lmengfan <mengfan.lou@gmail.com>
2018-12-10 12:49:07 -0500

    Version 0.02

---
+++ CustomExtension.extension/STVTools.tab/Clash Detection.panel/Clashmodes.pulldown/Declash Mode On.pushbutton/script.py
@@ -0,0 +1,137 @@
+from pyrevit.framework import List
+from pyrevit import revit, DB
+import clr, sys, re, os, imp
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
+clr.AddReference("System")
+from Autodesk.Revit.DB import FilteredElementCollector, Structure
+from Autodesk.Revit.DB import BuiltInCategory, ElementId, XYZ, Point, Transform, Transaction,FamilySymbol
+from System.Collections.Generic import List
+from Autodesk.Revit.UI import *
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.Creation import *
+from pyrevit import script
+from pyrevit import forms
+import pyrevit
+clr. AddReferenceByPartialName('PresentationCore')
+clr.AddReferenceByPartialName('PresentationFramework')
+clr.AddReferenceByPartialName('System.Windows.Forms')
+uidoc = __revit__.ActiveUIDocument
+doc = __revit__.ActiveUIDocument.Document
+
+# Get the right point data from the right direcory
+fName = doc.Title
+modelRegex = re.compile(r'\w\d\d\d\d\d\d\d\d-\S\S_CENTRAL')
+modelRegex2 = re.compile(r'\w\d\d\d\d\d\d\d\d-\S\S_CENTRAL_\w?\w?\w')
+approvedTail = ['ENC', 'FFE', 'GEN', 'INT', 'SSM', 'C', 'CP', 'PBB', ]
+noTail = modelRegex.findall(fName)
+tail = modelRegex2.findall(fName)
+if len(tail) == 0:
+    systemExtra = '\\\\stvgroup.stvinc.com\\v3\\DGPA\\Vol3\\Projects\\3019262\\3019262_0001\\90_CAD Models and Sheets\\17017000\\_PIM\\PointData\\' + fName[0: 20]
+    sys.path.append(systemExtra)
+    print(systemExtra)
+else:
+    nameLst = re.split('_', fName)
+    if nameLst[2] in approvedTail:
+        systemExtra = '\\\\stvgroup.stvinc.com\\v3\\DGPA\\Vol3\\Projects\\3019262\\3019262_0001\\90_CAD Models and Sheets\\17017000\\_PIM\\PointData\\' + tail[0]
+        sys.path.append(systemExtra)
+        print(systemExtra)
+    else:
+        systemExtra = '\\\\stvgroup.stvinc.com\\v3\\DGPA\\Vol3\\Projects\\3019262\\3019262_0001\\90_CAD Models and Sheets\\17017000\\_PIM\\PointData\\' + fName[0: 20]
+        sys.path.append(systemExtra)
+'''
+# date and time
+import datetime
+now = datetime.datetime.now()
+print(now.year + now.month)
+'''
+__doc__ = 'Select the shared point of the model '\
+          ' This is helpful check project info'
+# clash files
+clashFiles = []
+for file in os.listdir(systemExtra):
+    if file.endswith(".py"):
+        # print(os.path.join(clashpath, file))
+        __import__(file[:-3], locals(), globals())
+        clashFiles.append(file)
+sel_clash = forms.SelectFromList.show(clashFiles, button_name='Select Item',
+                                        multiselect=True,
+                                        name_attr='Name', )
+# Be sure to import the right one as in all point data files are named the same.
+
+
+# input ---------------------
+
+ew = doc.ActiveProjectLocation.GetProjectPosition(XYZ(0,0,0)).EastWest * float(-1.0)
+ns = doc.ActiveProjectLocation.GetProjectPosition(XYZ(0,0,0)).NorthSouth * float(-1.0)
+elevation = doc.ActiveProjectLocation.GetProjectPosition(XYZ(0,0,0)).Elevation * float(-1.0)
+angle = doc.ActiveProjectLocation.GetProjectPosition(XYZ(0,0,0)).Angle * float(-1.0)
+rotationTransform = Transform.CreateRotation(XYZ.BasisZ, angle)
+translationVector = XYZ(ew,ns,elevation)
+translationTransform = Transform.CreateTranslation(translationVector)
+finalTransform = translationTransform.Multiply(rotationTransform)
+count = 0
+outLst = []
+# Just to be sure how many points will be created and each list are the same length
+
+tab = '     '
+outprint = script.get_output()
+output = pyrevit.output.get_output()
+# Transaction Start
+t = Transaction(doc, 'Add CLash Points')
+
+# Clash point Calculation
+FSymbol = DB.FilteredElementCollector(doc) \
+    .OfClass(clr.GetClrType(FamilySymbol)) \
+    .ToElements()
+
+selSet = []
+for a in sel_clash:
+    Pointdata = imp.load_source('a', systemExtra + '\\' + a)
+    print('-----------------------------------------------------------------')
+    print('Here are all the clashes between ' + a[0: len(a) - 3])
+    clashPoint = ()
+    for i in FSymbol:
+        if i.Family.Name == 'Site-Generic-Clashpoint':
+            clashPoint = i
+    if not clashPoint:
+        print('Please Load the Clash Point Family')
+    else:
+        t.Start()
+        print('Using Clash Point Family: ' + clashPoint.Family.Name)
+        count = 0
+        elements = []
+        for el in Pointdata.pointX:
+            x = float(Pointdata.pointX[count]) + ew
+            y = float(Pointdata.pointY[count]) + ns
+            z = float(Pointdata.pointZ[count]) + elevation
+            clashName = str('No. ' + str(count + 1) + ' ID: ' + Pointdata.clashName[count])
+            clashwithID = str(Pointdata.otherFile[count] + ' ID: ' + Pointdata.clashwithID[count])
+            pnt = XYZ(x,y,z)
+            bPnt = Transform.CreateRotation(XYZ.BasisZ, angle).OfPoint(pnt)
+
+
+            # Clash point creation
+            boxes = doc.Create.NewFamilyInstance(bPnt, clashPoint, Structure.StructuralType.NonStructural)
+            elements.append(boxes)
+
+            boxes.LookupParameter('Clash Name').Set(clashName)
+            boxes.LookupParameter('Clash with ID').Set(clashwithID)
+            count += 1
+        t.Commit()
+
+
+        secondCount = 0
+        for el in elements:
+            selSet.append(el.Id)
+            idLen = len(Pointdata.clashName[secondCount])
+            elId = Pointdata.clashName[secondCount][2: idLen - 2]
+            idElement = ElementId(int(elId))
+            print ('Clash No. ' + str(secondCount + 1) + tab + 'Clash Point: ' + format(outprint.linkify(el.Id)) + tab +
+                   'Clash Item: ' + format(outprint.linkify(idElement)))
+            secondCount += 1
+
+revit.get_selection().set_to(selSet)
+
+# TODO: Select clashes to view in different colors look into select workset color change.
+
---
+++ CustomExtension.extension/STVTools.tab/Warnings.panel/User.pushbutton/script.py
@@ -0,0 +1,94 @@
+from pyrevit.framework import List
+from pyrevit import revit, DB
+import clr, pprint,os
+from collections import defaultdict
+from pyrevit import script
+from pyrevit import forms
+import pyrevit
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
+clr.AddReference("System")
+from Autodesk.Revit.DB import FilteredElementCollector
+from Autodesk.Revit.DB import BuiltInCategory, ElementId, XYZ, Point, Transform
+from System.Collections.Generic import List
+from Autodesk.Revit.UI import *
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.ApplicationServices import Application
+clr. AddReferenceByPartialName('PresentationCore')
+clr.AddReferenceByPartialName('PresentationFramework')
+clr.AddReferenceByPartialName('System.Windows.Forms')
+uidoc = __revit__.ActiveUIDocument
+doc = __revit__.ActiveUIDocument.Document
+
+
+__doc__ = 'Print Warning based on the selection of username.'\
+          ' This is intended to distribute warning responsibilities'
+
+outprint = script.get_output()
+output = pyrevit.output.get_output()
+tab = ' '
+user = Application.Username
+# input ---------------------
+cate = []
+path = 'C:\\Users\\loum\\Documents\\Pyscripts\\ClashScripts\\'
+if revit.doc.IsWorkshared:
+    warnings = doc.GetWarnings()
+    count = 1
+    warningRange = 0
+
+    for i in warnings:
+        id = i.GetFailingElements()
+        for i in id:
+            wti = DB.WorksharingUtils.GetWorksharingTooltipInfo(revit.doc, i)
+            owner = wti.Owner
+            creator = wti.Creator
+            if not owner in cate:
+                cate.append(owner)
+            if not creator in cate:
+                cate.append(creator)
+    # Select Warnings you want to print
+    sel_warning = forms.SelectFromList.show(cate, button_name='Select Item',
+                                            multiselect=False,
+                                            name_attr='Name',)
+    print('Below are all users in the model: ')
+    print(cate)
+    print('Below are selected user and warnings under his or her name: ')
+    print(sel_warning)
+    # Printing selected warnings
+    for warning in warnings:
+        elementId = warning.GetFailingElements()
+        additionalId = warning.GetAdditionalElements()
+        text = warning.GetDescriptionText()
+        names = []
+        for e in elementId:
+            wti = DB.WorksharingUtils.GetWorksharingTooltipInfo(revit.doc, e)
+            creator = wti.Creator
+            changedBy = wti.LastChangedBy
+            names.append(creator)
+            names.append(changedBy)
+        for a in additionalId:
+            awti = DB.WorksharingUtils.GetWorksharingTooltipInfo(revit.doc, a)
+            aCreator = awti.Creator
+            aChangedBy = awti.LastChangedBy
+            names.append(aCreator)
+            names.append(aChangedBy)
+        # print(names)
+        if sel_warning[0] in names:
+            output.print_md("**#** {}-----------------\n\n"
+                            "- Warning Item:{}\n\n"
+                            .format(count,
+                                    text))
+            for e in elementId:
+                wti = DB.WorksharingUtils.GetWorksharingTooltipInfo(revit.doc, e)
+                creator = wti.Creator
+                changedBy = wti.LastChangedBy
+                print(tab + format(outprint.linkify(e)) + tab + 'Creator: ' + creator + tab +
+                        'Last Changed by: ' + changedBy)
+            for a in additionalId:
+                awti = DB.WorksharingUtils.GetWorksharingTooltipInfo(revit.doc, a)
+                aCreator = awti.Creator
+                aChangedBy = awti.LastChangedBy
+                print(tab + format(outprint.linkify(a)) + tab + 'Creator: ' + aCreator + tab +
+                        'Last Changed by: ' + aChangedBy)
+            count += 1
+        warningRange += 1
