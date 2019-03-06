fd57d6d5dd4070f47893d7440db4913bb1db166c
lmengfan <mengfan.lou@gmail.com>
2019-03-06 13:47:02 -0500

    Added tge select all instance under family tool; Added the Rotate pipe Fitting based on pipe axis tool.

---
+++ CustomExtension.extension/STVTools.tab/General Utilities.panel/Selection.pulldown/Select all item of family instance.pushbutton/script.py
@@ -0,0 +1,38 @@
+from pyrevit.framework import List
+from pyrevit import revit, DB
+import clr
+
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitAPIUI')
+clr.AddReference("System")
+from Autodesk.Revit.DB import FilteredElementCollector
+from Autodesk.Revit.DB import BuiltInCategory, ElementId, XYZ, ExternalFileReference,FamilyInstance,ElementParameterFilter
+from System.Collections.Generic import List
+from Autodesk.Revit.UI.Selection import ObjectType
+clr. AddReferenceByPartialName('PresentationCore')
+clr.AddReferenceByPartialName('PresentationFramework')
+clr.AddReferenceByPartialName('System.Windows.Forms')
+uidoc = __revit__.ActiveUIDocument
+doc = __revit__.ActiveUIDocument.Document
+selection = [doc.GetElement(id)
+            for id in __revit__.ActiveUIDocument.Selection.GetElementIds()]
+
+__doc__ = 'Select the shared point of the model '\
+          'This is helpful check project info'
+
+# containment -----------------------------------
+choices = uidoc.Selection
+ref = choices.PickObject(ObjectType.Element, "Pick Element")
+ele = doc.GetElement(ref.ElementId)
+fType = ele.Symbol.Family.Name
+print(fType)
+Obj = FilteredElementCollector(doc).OfClass(FamilyInstance).ToElements()
+selec = []
+for i in Obj:
+	if i.Symbol.Family.Name == fType:
+		selec.append(i)
+revit.get_selection().set_to(selec)
+
+
+
+
--- CustomExtension.extension/STVTools.tab/MEP Tools.panel/Rotate Fitting.pushbutton/script.py
+++ CustomExtension.extension/STVTools.tab/MEP Tools.panel/Rotate Fitting.pushbutton/script.py
@@ -4,7 +4,7 @@ clr.AddReference('RevitAPIUI')
 clr.AddReference("System")
 from Autodesk.Revit.DB import Document, FilteredElementCollector, PerformanceAdviser, FamilySymbol,BuiltInCategory, \
     MEPCurveType, RoutingPreferenceRuleGroupType, RoutingConditions, RoutingPreferenceErrorLevel, RoutingPreferenceRule,\
-    PreferredJunctionType,Transaction, BuiltInParameter, XYZ, Line
+    PreferredJunctionType,Transaction, BuiltInParameter, XYZ, Line, Connector
 from Autodesk.Revit.UI.Selection import ObjectType
 from Autodesk.Revit.DB.Plumbing import PipeType
 clr. AddReferenceByPartialName('PresentationCore')
@@ -14,59 +14,58 @@ uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
 from pyrevit.framework import List
 from pyrevit import revit, DB
-import os
 from collections import defaultdict
 from pyrevit import script
 from pyrevit import forms
-__doc__ = 'Switch Elbow Preference of selected Pipe'\
-          'You can either select the pipe you want to switch ' \
-          'or pick from list'
+
+
+__doc__ = 'Rotate Pipe Fittings connected to pipe'\
+          'Click the tool, select the pipe and then the fitting ' \
+          'Type in a number you want to rotate and you are good to go,'\
+          'Can Take negative Values'
 choices = uidoc.Selection
 pipeRef = choices.PickObject(ObjectType.Element, "Pick Pipe")
 connectorRef = choices.PickObject(ObjectType.Element, "Pick Pipe Fitting")
 pipe = doc.GetElement(pipeRef.ElementId)
-connector = doc.GetElement(pipeRef.ElementId)
-print(pipeRef.ElementId)
-print(connectorRef.ElementId)
-conns = []
-def disconnect(curve, list):
-
-	connectors = curve.ConnectorManager.Connectors
-	for conn in connectors:
-		if conn.IsConnected:
-			for c in conn.AllRefs:
-				if conn.IsConnectedTo(c):
-					list.append(conn)
-					list.append(c)
-					conn.DisconnectFrom(c)
-
+connector = doc.GetElement(connectorRef.ElementId)
 angleInput = str(forms.GetValueWindow.show(None,
         value_type='string',
         default=str(0.00),
         prompt='Please Enter angle',
         title='Angle'))
-angle = float(angleInput)
+angle = (float(angleInput)*math.pi)/180
 t = Transaction(doc, 'Rotate Fitting')
 t.Start()
-primary = ()
-connectors = connector.ConnectorManager.Connectors
-print(connectors)
+inn = [connector,pipe]
+conns = [0,1]
+pointOne = pipe.Location.Curve.GetEndPoint(0)
+pointTwo = pipe.Location.Curve.GetEndPoint(1)
+
+def disconnect(curve):
+	connectors = curve.ConnectorManager.Connectors
+	for conn in connectors:
+		if conn.IsConnected:
+			for c in conn.AllRefs:
+				if conn.IsConnectedTo(c):
+					conns[0] = conn
+					conns[1] = c
+					conn.DisconnectFrom(c)
+
+connectors = inn[0].MEPModel.ConnectorManager.Connectors
 for c in connectors:
 	if c.GetMEPConnectorInfo().IsPrimary:
 		primary = c
 		break
 	primary = c
-print(primary)
-loc = connector.Location
-ductloc = pipe.Location
-center = primary.CoordinateSystem.BasisZ
-centerrot = XYZ(center.X,center.Y,center.Z)
-line = Line.CreateUnbound(primary.Origin,center)
-disconnect(connector, conns)
+loc = inn[0].Location
+ductloc = inn[1].Location
+# center = primary.CoordinateSystem.BasisZ
+# centerrot = XYZ(center.X,center.Y,center.Z)
+center = primary.Origin
+line = DB.Line.CreateBound(pointOne, pointTwo)
+disconnect(inn[1])
 ductloc.Rotate(line,angle)
-loc.Rotate(line,angle)
-print(conns[0])
-print(conns[1])
+success = loc.Rotate(line,angle)
 conns[0].ConnectTo(conns[1])
 
 t.Commit()
