531bdca13323df4accab6424a411e6c2e4db0df7
lmengfan <mengfan.lou@gmail.com>
2019-08-19 15:30:20 -0400

    developing conduit tools

---
+++ CustomExtension.extension/STVTools.tab/EWR QAQC.panel/QAQC.pulldown/LOD - QAQC.pushbutton/script.py
@@ -0,0 +1,102 @@
+import sys
+import ConfigParser
+from os.path import expanduser
+# Set system path
+home = expanduser("~")
+cfgfile = open(home + "\\STVTools.ini", 'r')
+config = ConfigParser.ConfigParser()
+config.read(home + "\\STVTools.ini")
+# Master Path
+syspath1 = config.get('SysDir','MasterPackage')
+sys.path.append(syspath1)
+# Built Path
+syspath2 = config.get('SysDir','SecondaryPackage')
+sys.path.append(syspath2)
+from pyrevit.framework import List
+from pyrevit import revit, DB, forms
+import re, clr, os, threading
+import EwrQcUtils
+import xlsxwriter
+clr.AddReference('RevitAPI')
+clr.AddReference("System")
+from Autodesk.Revit.DB import FilteredElementCollector, Transaction, ImportInstance, \
+	OpenOptions,WorksetConfiguration, WorksetConfigurationOption, DetachFromCentralOption,\
+    ModelPathUtils, SaveAsOptions, WorksharingSaveAsOptions
+from System.Collections.Generic import List
+from Autodesk.Revit.UI.Events import DialogBoxShowingEventArgs
+from Autodesk.Revit.UI import UIApplication
+from Autodesk.Revit.ApplicationServices import Application
+clr.AddReferenceByPartialName('PresentationCore')
+clr.AddReferenceByPartialName('PresentationFramework')
+clr.AddReferenceByPartialName('System.Windows.Forms')
+clr.AddReference('RevitAPIUI')
+# Collect Save location and Rvt Files
+collectorFiles = forms.pick_file(file_ext='rvt', multi_file=True, unc_paths=False)
+destinationFolder = forms.pick_folder()
+
+def RVTFileCollector(dir):
+    files = []
+    for file in os.listdir(dir):
+        if file.endswith(".rvt"):
+            #print(str(file))
+            files.append(str(file))
+    print files
+    return files
+
+def OpenFiles(oFile, app, audit):
+
+    openOpt = OpenOptions()
+    if audit == True:
+        openOpt.Audit = True
+    else:
+        openOpt.Audit = False
+    openOpt.DetachFromCentralOption = DetachFromCentralOption.DetachAndPreserveWorksets
+    wsopt = WorksetConfiguration(WorksetConfigurationOption.CloseAllWorksets)
+    # wsopt.Open(worksetList)
+    openOpt.SetOpenWorksetsConfiguration(wsopt)
+    modelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(oFile)
+    currentdoc = app.OpenDocumentFile(modelPath, openOpt)
+    try:
+        DialogBoxShowingEventArgs.OverrideResult(1)
+    except:
+        pass
+    return currentdoc
+
+
+# Main
+uidoc = __revit__.ActiveUIDocument
+doc = __revit__.ActiveUIDocument.Document
+
+__doc__ = 'Open projects and resave in a specific location'\
+            'Please do not use lightly'
+uiapp = UIApplication(doc.Application)
+application = uiapp.Application
+
+def ElementsProcessing(openedDoc, name):
+    collectorFamily = EwrQcUtils.ElementinWorksetCheck(openedDoc)
+    EwrQcUtils.ExcelWriter(excelFile, name, 1, 0, collectorFamily)
+
+# Transaction
+if len(collectorFiles) > 0:
+    t = Transaction(doc, 'Check QAQC Elements')
+    t.Start()
+    fileName = destinationFolder + '\\' + 'LOD Check File' + '.xlsx'
+    excelFile = EwrQcUtils.ExcelOpener(fileName)
+    for aDoc in collectorFiles:
+        openedDoc = OpenFiles(aDoc, application, audit = False)
+        print(str(openedDoc.Title) + ' Opened')
+        workshareOp = WorksharingSaveAsOptions()
+        # Define the name and location of excel file
+        rawTitle = re.split('detached', openedDoc.Title)[0]
+        title = rawTitle[0:len(rawTitle) -1]
+
+        # Define and Open Excel File
+        threading.Thread(name=title, target=ElementsProcessing(openedDoc, title))
+        # Close Excel and Revit File
+        openedDoc.Close(False)
+    excelFile.close()
+    print('File Saved' )
+    t.Commit()
+else:
+    forms.alert('No File is selected', title='', sub_msg=None, expanded=None, footer='', ok=True, cancel=False, yes=False,
+                        no=False, retry=False, warn_icon=True, options=None, exitscript=False)
