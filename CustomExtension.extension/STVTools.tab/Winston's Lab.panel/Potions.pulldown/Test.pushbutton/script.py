0d321982d2775a8dc01fe6dec3793845b064fe00
lmengfan <mengfan.lou@gmail.com>
2019-09-04 10:40:30 -0400

    Update

--- CustomExtension.extension/STVTools.tab/Winston's Lab.panel/Potions.pulldown/Test.pushbutton/script.py
+++ CustomExtension.extension/STVTools.tab/Winston's Lab.panel/Potions.pulldown/Test.pushbutton/script.py
@@ -34,21 +34,30 @@ import os
 from collections import defaultdict
 from pyrevit import script
 from pyrevit import forms
-def RenameViews(doc):
-    views = FilteredElementCollector(doc).OfClass(View).ToElements()
-    names = []
+def get_selected_elements(doc):
+    """API change in Revit 2016 makes old method throw an error"""
+    try:
+        # Revit 2016
+        return [doc.GetElement(id)
+                for id in __revit__.ActiveUIDocument.Selection.GetElementIds()]
+    except:
+        # old method
+        return list(__revit__.ActiveUIDocument.Selection.Elements)
 
-    for i in views:
-        name = i.ViewName
-        if name[0:3] == 'SQ-':
-            i.ViewName = name[3:]
+selection = get_selected_elements(doc)
 
-selection =  FilteredElementCollector(doc).OfClass(ViewSchedule).ToElements()
-t = Transaction(doc, 'Change Level Name')
+FSymbol = DB.FilteredElementCollector(doc) \
+    .OfClass(clr.GetClrType(FamilySymbol)) .OfCategory(BuiltInCategory.OST_GenericAnnotation)\
+    .ToElements()
+for i in FSymbol:
+    if i.Family.Name == 'Site-Generic-Clashpoint':
+        clashPoint = i
+
+t = Transaction(doc, 'Replace dwg')
 t.Start()
-RenameViews(doc)
-print("Hellow World")
-
+for cad in selection:
+    location = cad.Location
+    boxes = doc.Create.NewFamilyInstance(bPnt, clashPoint, Structure.StructuralType.NonStructural)
 t.Commit()
 
 
